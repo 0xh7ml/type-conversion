e11c04b1a7f5e2a41bbede71027932e32d0bbced
Rudra Sarkar <rudrasarkar815@gmail.com>
2019-05-29 15:30:36 +0600

    Python Type Conversion Problem

---
+++ exercise_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# define savings and result
+
+savings = 100
+result  = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I Started saving with $" + saving + " and now have $" + result + ". Great!")
+
+# Current money in bank
+money_str = "1337.37"
+
+# Convert money_str into float: money_float
+
+
+# printout
